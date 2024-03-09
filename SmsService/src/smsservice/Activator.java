e553de4be145d6d686e5d38877d79b8c6f1a81d0
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2024-03-10 02:30:13 +0530

    exposedSmsService

--- SmsService/src/smsservice/Activator.java
+++ SmsService/src/smsservice/Activator.java
@@ -6,17 +6,19 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
 import smsservice.implemention.SmsServiceImpl;
+import smsservice.service.SmsService;
 
 public class Activator implements BundleActivator {
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_YELLOW = "\u001B[33m";
-	private ServiceRegistration<?> SmsService;
+	private ServiceRegistration<?> SmsServiceRegister;
 
 
 	public void start(BundleContext bundleContext) throws Exception {
 		System.out.println(ANSI_YELLOW + "Starting SMS Service" + ANSI_RESET);
 		SmsServiceImpl smsServiceImpl = new SmsServiceImpl();
-		smsServiceImpl.SendSms();
+//		smsServiceImpl.SendSms();
+		SmsServiceRegister = bundleContext.registerService(SmsService.class.getName(), smsServiceImpl, null);
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
